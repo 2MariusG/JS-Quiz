9c5c27cf51cc8b77c719344fa3ac95915fd83941
2MariusG <73899587+2MariusG@users.noreply.github.com>
2021-11-03 11:16:13 +0200

    score count

---
+++ app.js
@@ -0,0 +1,28 @@
+const correctAnswers = ['B', 'B', 'B', 'B',];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    
+    //check answers
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score += 25;
+       }
+    });
+
+    //show results on page
+    scrollTo(0, 0);
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+});
+
+//window object (global object)
+
+console.log('hello');
